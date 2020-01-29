2fecfdc434c3bd6f8dc82188a3e2bcd9e9f5d72d
stefaanhaeck <43784442+stefaanhaeck@users.noreply.github.com>
2020-01-29 10:26:35 +0100

    commit

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,7 +15,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count <= 5) {
+            if (Students.Count < 5) {
                 throw new InvalidOperationException();
             }
 
