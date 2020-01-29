4783cb9ffd5998ae592b9289830afd302e421e00
stefaanhaeck <43784442+stefaanhaeck@users.noreply.github.com>
2020-01-29 11:15:36 +0100

    support for weighted grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
-            Name = name;
+            this.Name = name;
             Students = new List<Student>();
+            this.IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -105,20 +107,37 @@ namespace GradeBook.GradeBooks
             }
         }
 
-        public virtual double GetGPA(char letterGrade, StudentType studentType)
+        public virtual double GetGPA(char letterGrade, StudentType studentType, bool weighted)
         {
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    if ((weighted && studentType == StudentType.Honors) || (weighted && studentType == StudentType.DualEnrolled))
+                        return 5;
+                    else
+                        return 4;
                 case 'B':
-                    return 3;
+                    if ((weighted && studentType == StudentType.Honors) || (weighted && studentType == StudentType.DualEnrolled))
+                        return 4;
+                    else
+                        return 3;
                 case 'C':
-                    return 2;
+                    if ((weighted && studentType == StudentType.Honors) || (weighted && studentType == StudentType.DualEnrolled))
+                        return 3;
+                    else
+                        return 2;
                 case 'D':
-                    return 1;
+                    if ((weighted && studentType == StudentType.Honors) || (weighted && studentType == StudentType.DualEnrolled))
+                        return 2;
+                    else
+                    {
+                        return 1;
+                    };
                 case 'F':
-                    return 0;
+                    if ((weighted && studentType == StudentType.Honors) || (weighted && studentType == StudentType.DualEnrolled))
+                        return 1;
+                    else
+                        return 0;
             }
             return 0;
         }
