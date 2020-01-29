15cee67272088f871f22d63881160f7439b04a13
stefaanhaeck <43784442+stefaanhaeck@users.noreply.github.com>
2020-01-29 10:23:47 +0100

    commit

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,22 +15,22 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            /*if (Students.Count <= 5) {
+            if (Students.Count <= 5) {
                 throw new InvalidOperationException();
             }
 
             var treshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(x => x.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[treshold - 1] <= averageGrade)            
+            /*if (grades[treshold - 1] <= averageGrade)            
                 return 'A';
             else if (grades[(treshold * 2) - 1] <= averageGrade)
                 return 'B';
             else if (grades[(treshold * 3) - 1] <= averageGrade)
                 return 'C';
             else if (grades[(treshold * 4) - 1] <= averageGrade)
-                return 'D';
-                */
+                return 'D';*/
+                
             return 'K';
         }
     }
